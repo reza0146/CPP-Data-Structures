17682d28905f43ea734af5c804322a253295d647
Reza Partovi <reza.partovi96@gmail.com>
2023-10-20 21:52:36 -0700

    Changed formatting

--- Hash Map/map_practice.hpp
+++ Hash Map/map_practice.hpp
@@ -1,11 +1,12 @@
+/*
+Used a template to be able pass both unordered and ordered maps. 
 
-// Used a template to be able pass both unordered and ordered maps. 
+The elements of a map are of type std::pair. Instead of auto keyword we could use std::pair<std::string, int>.
 
-// The elements of a map are of type std::pair. Instead of auto keyword we could use std::pair<std::string, int>.
-
-// The for-each loop provides a convenient syntax for traversing a hash map. In a for-each loop, the counter variable is the hash map element itself, 
-// which is an object of type std::pair. std::pair has two member variables: 
-// .first is the value of the key and .second is the mapped value
+The for-each loop provides a convenient syntax for traversing a hash map. In a for-each loop, the counter variable is the hash map element itself, 
+which is an object of type std::pair. std::pair has two member variables: 
+.first is the value of the key and .second is the mapped value
+*/
 
 template <typename T>
 void ShowMapInfo(T the_map) {
